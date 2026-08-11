7d0ea53e9dcd8f23fba3ed38f1ede3e6f71b5728
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-08-12 00:22:53 +0100

    Add files via upload

---
+++ Kernel.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+
+namespace CosmosBall
+{
+    public class Kernel : Sys.Kernel
+    {
+
+        protected override void BeforeRun()
+        {
+            Console.WriteLine("Cosmos booted successfully. Type a line of text to get it echoed back.");
+        }
+
+        protected override void Run()
+        {
+            ball.Main();
+            var input = Console.ReadLine();
+            while (true) {; }
+        }
+    }
+    class balls
+    {
+        public static int w = 79;
+        public static int h = 23;
+        public static int y = 2;
+        public static int x = 2;
+        public static int movx = 1;
+        public static int movy = 1;
+
+        public static void Writer(int xx, int yy, String s)
+        {
+            Console.CursorLeft = xx;
+            Console.CursorTop = yy;
+            Console.WriteLine(s);
+        }
+        public static void nexts()
+        {
+            Writer(x, y, " ");
+            x = x + movx;
+            y = y + movy;
+            if (x < 1 || x > w) movx = -movx;
+            if (y < 1 || y > h) movy = -movy;
+            Writer(x, y, "O");
+
+        }
+
+        public static void ballsLoop(int xx, int yy)
+        {
+            if (xx > 1) w = xx;
+            if (yy > 1) h = yy;
+            while (true)
+
+            {
+                nexts();
+                System.Threading.Thread.Sleep(500);
+                if (Console.KeyAvailable)
+                {
+                    break;
+                }
+            }
+
+        }
+
+    }
+
+
+
+
+    class ball
+    {
+        public static void Main()
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+            balls.ballsLoop(Console.WindowWidth - 3, Console.WindowHeight - 3);
+        }
+
+    }
+
+
+}
